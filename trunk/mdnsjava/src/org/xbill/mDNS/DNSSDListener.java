4386959e697b5601f3bf867b423a091f415f11e3
posicks@gmail.com <posicks@gmail.com@affaffa0-e86d-2f1f-f803-8501c23130cb>
2014-02-07 18:23:27 +0000

    Moved Service Discovery Search from Browse class to MulticastDNSService Class, so that it is with the service registration code.
    Change Browse to perform asynchronous DNS style mDNS browsing for any mDNS records, not just services.

--- trunk/mdnsjava/src/org/xbill/mDNS/DNSSDListener.java
+++ trunk/mdnsjava/src/org/xbill/mDNS/DNSSDListener.java
@@ -1,11 +1,16 @@
 package org.xbill.mDNS;
 
+import org.xbill.DNS.Message;
+
 public interface DNSSDListener
 {
     public void serviceDiscovered(Object id, ServiceInstance service);
     
     
     public void serviceRemoved(Object id, ServiceInstance service);
+    
+    
+    public void receiveMessage(Object id, Message m);
     
     
     public void handleException(Object id, Exception e);
